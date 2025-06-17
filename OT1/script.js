258f20c58f0052fa9c4924baa0002745346b62a5
kadnys_martins <kadnysm@gmail.com>
2025-06-17 20:40:25 -0300

    First commit

---
+++ OT1/script.js
@@ -0,0 +1,5 @@
+const meuNome = "Seu Nome Aqui";
+const minhaIdade = 30;
+const comidaFavorita = "Pizza";
+
+console.log("Meu nome é " + meuNome + ", tenho " + minhaIdade + " anos e minha comida favorita é " + comidaFavorita + ".");
