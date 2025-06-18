4730d5d5f17ef8308d88d2222de4e7c2037bc515
kadnys_martins <kadnysm@gmail.com>
2025-06-17 21:11:47 -0300

    Adicionando a extensão Quokka

--- OT1/script.js
+++ OT1/script.js
@@ -1,5 +1,8 @@
-const meuNome = "Seu Nome Aqui";
-const minhaIdade = 30;
-const comidaFavorita = "Pizza";
+const meuNome = "Kadnys";
+let minhaIdade = 21;
+let comidaFavorita = "Carne de Panela";
+let n1 = 123;
+let n2 = 5;
+
 
 console.log("Meu nome é " + meuNome + ", tenho " + minhaIdade + " anos e minha comida favorita é " + comidaFavorita + ".");
