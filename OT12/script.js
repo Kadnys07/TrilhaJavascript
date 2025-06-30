0cfb5e122a4468e84de442c111d68ee0794efe3b
kadnys_martins <kadnysm@gmail.com>
2025-06-30 20:42:24 -0300

    OT11 em andamento

---
+++ OT12/script.js
@@ -0,0 +1,24 @@
+let frutas = ["maçã", "banana", "laranja", "uva"];
+let carnes = ["frango", "bife", "peixe"];
+let compras = frutas.concat(carnes);
+console.log(compras);
+
+// Usuario:
+let usuario = {nome:"Jonas", email:"Jonas@gmail.com" };
+let novoUsuario = { ...usuario, status: "ativo" };
+console.log(novoUsuario);
+
+// Festa de Aniversário
+let convidados = ["Ana", "Bruno", "Carlos"];
+
+function listarConvidados(convidados) {
+    return `Convidados: ${convidados.join(", ")}`;
+}
+console.log(listarConvidados(convidados));
+
+// Musicas Array
+
+let musicas = ["Bohemian Rhapsody", "Imagine", "Hotel California"];
+let cloneMusicas = [...musicas, "Stairway to Heaven", "Smells Like Teen Spirit"];
+console.log(musicas);
+console.log(cloneMusicas);
